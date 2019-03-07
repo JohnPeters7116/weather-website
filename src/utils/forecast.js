1a5ab5e68d32d98dba5cf0b7bac2b84fdbe0ce4c
John Peters <johnpeters7116@gmail.com>
2019-03-07 07:38:50 +0000

    removed API keys and replaces with secure ENV variables

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,8 +1,9 @@
 const request = require('request')
+const KEY = process.env.DARKSKY_KEY
 
 const forecast = (coordinates, callback) => {
   const {lat, long} = coordinates
-  const url = `https://api.darksky.net/forecast/adc2830da015117154ddbd2c4f551bd6/${lat},${long}`
+  const url = `https://api.darksky.net/forecast/${KEY}/${lat},${long}`
 
   request({url, json: true}, (error, { body }) => {
     if (error){
